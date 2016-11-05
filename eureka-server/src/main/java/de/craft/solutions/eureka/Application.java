3587c36d10a02aebd0f78b1d39fc82d0ca08b72a
Alen Tolj <alen.tolj@comsysto.com>
2016-11-05 11:05:28 +0100

    Bootstrapped eureka server.

--- eureka-server/src/main/java/de/craft/solutions/eureka/Application.java
+++ eureka-server/src/main/java/de/craft/solutions/eureka/Application.java
@@ -3,7 +3,7 @@ package de.craft.solutions.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  *
  * @author Gunnar Hillert
@@ -15,7 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
